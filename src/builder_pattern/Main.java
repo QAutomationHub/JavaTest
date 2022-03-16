f281ae8e18e6365f35732824e03f8176e08ee53f
Strelinov Alexandr <strelinov@yandex.ru>
2022-03-16 09:27:30 +0300

    commit1

---
+++ src/builder_pattern/Main.java
@@ -0,0 +1,10 @@
+package BuilderPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8).calories(100).sodium(35).carbohydrate(27).build();
+
+        
+
+    }
+}
